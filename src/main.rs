38ce9a513452682e1ed45a840a161e3f8cb48cba
Aser Garcia <aser.garcia@outlook.com>
2026-03-15 21:31:37 -0400

    feat: model training

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,69 @@
-use burn::backend::{NdArray, ndarray::NdArrayDevice};
+use burn::backend::{Autodiff, NdArray, ndarray::NdArrayDevice};
+use burn::config::Config;
 use burn::data::dataloader::DataLoaderBuilder;
+use burn::optim::AdamConfig;
+use burn::prelude::*;
+use burn::record::CompactRecorder;
+use burn::train::{
+    Learner, SupervisedTraining,
+    metric::{AccuracyMetric, LossMetric},
+};
+
 use mnist::data::{MnistBatcher, MnistDataset};
+use mnist::model::ModelConfig;
+
+#[derive(Config, Debug)]
+pub struct TrainingConfig {
+    pub model: ModelConfig,
+    pub optimizer: AdamConfig,
+    #[config(default = 10)]
+    pub num_epochs: usize,
+    #[config(default = 64)]
+    pub batch_size: usize,
+    #[config(default = 4)]
+    pub num_workers: usize,
+    #[config(default = 42)]
+    pub seed: u64,
+    #[config(default = 1.0e-4)]
+    pub learning_rate: f64,
+}
 
 fn main() {
+    type TrainAutodiffBackend = Autodiff<NdArray>;
     // load the dataset
     let batch_size = 4;
     let num_workers = 4;
 
     let device = NdArrayDevice::default();
 
-    let batcher = MnistBatcher::<NdArray>::new(device);
-    let dataloader = DataLoaderBuilder::new(batcher)
+    let config = TrainingConfig::new(ModelConfig::new(10, 256), AdamConfig::new());
+
+    let batcher = MnistBatcher::<TrainAutodiffBackend>::new(device);
+    let test_batcher = MnistBatcher::<NdArray>::new(device);
+    let dataloader_train = DataLoaderBuilder::new(batcher)
         .batch_size(batch_size)
+        .shuffle(config.seed)
         .num_workers(num_workers)
         .build(MnistDataset::train());
 
-    for (it, batch) in dataloader.iter().enumerate() {
-        println!(
-            "[Iteration {}] Images {:?} | Targets {:?}",
-            it,
-            batch.images.dims(),
-            batch.targets.dims()
-        );
-    }
+    let dataloader_test = DataLoaderBuilder::new(test_batcher)
+        .batch_size(batch_size)
+        .shuffle(config.seed)
+        .num_workers(num_workers)
+        .build(MnistDataset::test());
+
+    let training = SupervisedTraining::new("checkpoints", dataloader_train, dataloader_test)
+        .metrics((AccuracyMetric::new(), LossMetric::new()))
+        .with_file_checkpointer(CompactRecorder::new())
+        .num_epochs(config.num_epochs)
+        .summary();
+
+    let model = config.model.init::<TrainAutodiffBackend>(&device);
+    let result = training.launch(Learner::new(
+        model,
+        config.optimizer.init(),
+        config.learning_rate,
+    ));
+
+    result.model.save_file("models", &CompactRecorder::new());
 }
---
+++ src/model.rs
@@ -0,0 +1,86 @@
+use burn::nn::{
+    Relu,
+    loss::{CrossEntropyLoss, CrossEntropyLossConfig},
+    modules::{
+        Linear, LinearConfig,
+        conv::{Conv2d, Conv2dConfig},
+    },
+};
+use burn::prelude::*;
+use burn::tensor::backend::AutodiffBackend;
+use burn::train::{ClassificationOutput, InferenceStep, TrainOutput, TrainStep};
+
+use crate::data::MnistBatch;
+
+#[derive(Config, Debug)]
+pub struct ModelConfig {
+    num_classes: usize,
+    hidden_size: usize,
+}
+
+impl ModelConfig {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Model<B> {
+        Model {
+            // 1 channel in, 1 channel out (feature map)
+            conv1: Conv2dConfig::new([1, 1], [3, 3]).init(device),
+            // 1 channel, 26Wx26H
+            linear1: LinearConfig::new(1 * 26 * 26, self.hidden_size).init(device),
+            linear2: LinearConfig::new(self.hidden_size, self.num_classes).init(device),
+            activation: Relu::new(),
+        }
+    }
+}
+
+#[derive(Module, Debug)]
+pub struct Model<B: Backend> {
+    conv1: Conv2d<B>,
+    linear1: Linear<B>,
+    linear2: Linear<B>,
+    activation: Relu,
+}
+
+impl<B: Backend> Model<B> {
+    pub fn forward(&self, images: Tensor<B, 3>) -> Tensor<B, 2> {
+        let [batch_size, height, width] = images.dims();
+
+        let x = images.reshape([batch_size, 1, height, width]);
+
+        let x = self.conv1.forward(x); // batch_size, 28, _, _
+        let x = self.activation.forward(x);
+        let x = x.reshape([batch_size, 1 * 26 * 26]);
+        let x = self.linear1.forward(x);
+        let x = self.activation.forward(x);
+        self.linear2.forward(x)
+    }
+
+    pub fn forward_classification(
+        &self,
+        images: Tensor<B, 3>,
+        targets: Tensor<B, 1, Int>,
+    ) -> ClassificationOutput<B> {
+        let output = self.forward(images);
+        let loss = CrossEntropyLossConfig::new()
+            .init(&output.device())
+            .forward(output.clone(), targets.clone());
+        ClassificationOutput::new(loss, output, targets)
+    }
+}
+
+impl<B: AutodiffBackend> TrainStep for Model<B> {
+    type Input = MnistBatch<B>;
+    type Output = ClassificationOutput<B>;
+
+    fn step(&self, batch: MnistBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
+        let item = self.forward_classification(batch.images, batch.targets);
+        TrainOutput::new(self, item.loss.backward(), item)
+    }
+}
+
+impl<B: Backend> InferenceStep for Model<B> {
+    type Input = MnistBatch<B>;
+    type Output = ClassificationOutput<B>;
+
+    fn step(&self, batch: MnistBatch<B>) -> ClassificationOutput<B> {
+        self.forward_classification(batch.images, batch.targets)
+    }
+}
